37ee785ad3afdb24df03b82f179593fb5592c493
temPirati <ahmet77meti@gmail.com>
2022-06-20 17:36:13 +0200

    settings fix

--- intern_backend/settings.py
+++ intern_backend/settings.py
@@ -119,11 +119,11 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.AllowAny',
     ],
-    # 'DEFAULT_AUTHENTICATION_CLASSES': (
+    'DEFAULT_AUTHENTICATION_CLASSES': (
     #     'rest_framework_simplejwt.authentication.JWTAuthentication',
-    #     'rest_framework.authentication.BasicAuthentication',
-    #     'rest_framework.authentication.SessionAuthentication',
-    # ),
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ),
     'DEFAULT_RENDERER_CLASSES': [
             'rest_framework.renderers.JSONRenderer',
 
